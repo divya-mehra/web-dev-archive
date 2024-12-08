79dfeb69cd9cab64dbb82f3952c4dd782f8a77fb
Divya Mehra <divya.w.mehra@gmail.com>
2024-12-08 22:13:18 +0530

    nav and sections

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 let markupToggle = document.querySelector("#markup-toggle");
-let markupElements = document.querySelectorAll(".markup")
+let markupElements = document.querySelectorAll(".markup");
 
 let toggleMarkup = () => {
-    if(markupToggle.classList.contains("show")) {
-        markupToggle.classList.toggle("show");
-        markupToggle.textContent = "Show Markup"
-        markupElements.forEach((el) => {el.style.display = "none"})
-    } else {
-        markupToggle.classList.toggle("show");
-        markupElements.forEach((el) => {el.style.display = "inline-block"})
-        markupToggle.textContent = "Hide Markup"
+  if (markupToggle.classList.contains("show")) {
+    markupToggle.classList.toggle("show");
+    markupToggle.textContent = "Show Markup";
+    markupElements.forEach((el) => {
+      el.style.display = "none";
+    });
+  } else {
+    markupToggle.classList.toggle("show");
+    markupElements.forEach((el) => {
+      el.style.display = "inline-block";
+    });
+    markupToggle.textContent = "Hide Markup";
+  }
+};
+
+markupToggle.addEventListener("click", toggleMarkup);
+
+// Change sections
+
+let sections = ["agenda", "syllabus"];
+
+let changeSection = (element) => {
+  element.target.classList.toggle("selected-link");
+  let el = document.querySelector(`#${element.target.id}-content`);
+  el.classList.toggle("show");
+  document.querySelectorAll(".main-content").forEach((e) => {
+    if (e != el) {
+      e.classList.remove("show");
     }
-}
+  });
+  document.querySelectorAll(".nav-link").forEach((e) => {
+    if (e != element.target) {
+      e.classList.remove("selected-link");
+    }
+  });
+};
 
-markupToggle.addEventListener("click", toggleMarkup)
-
+document.querySelector("#agenda").addEventListener("click", (e) => {
+  changeSection(e);
+});
+document.querySelector("#syllabus").addEventListener("click", (e) => {
+  changeSection(e);
+});
